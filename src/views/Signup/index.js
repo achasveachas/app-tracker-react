326bcae86edd23a9014e806583bc0c1d44f2cbe6
Yechiel K <chilikkal@gmail.com>
2017-05-02 01:34:33 -0400

    added sign up form

--- src/views/Signup/index.js
+++ src/views/Signup/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
-const Signup = () => <form className="signup_form">Signup</form>
+const Signup = () =>
+  <form className="signup_form">
+    <input type="text" placeholder="Username" />
+    <input type="password" placeholder="Password" />
+    <input type="submit" value="Create User" />
+  </form>
 
 export default Signup;
