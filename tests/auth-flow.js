be95918465a1bc5714ddb198f9a3159807c55350
Yechiel K <chilikkal@gmail.com>
2017-05-02 01:34:11 -0400

    added test for sign up functionality

--- tests/auth-flow.js
+++ tests/auth-flow.js
@@ -1,18 +1,28 @@
 module.exports = {
-  'get to signup page': (client) => {
+  'get to signup page': (browser) => {
 
-    client
-      .url(client.launchUrl)
+    browser
+      .url(browser.launchUrl)
       .waitForElementVisible('.navbar', 1000)
       .click('a[href="/signup"]')
 
-      client.assert.urlContains('signup')
-      client.assert.cssClassPresent('form','signup_form')
+      browser.assert.urlContains('signup')
+      browser.assert.cssClassPresent('form','signup_form')
   },
 
-  'signup user and redirect to applications': (client) => {
+  'signup user and redirect to applications': (browser) => {
 
+    browser
+      .setValue('input[type=text]', 'testuser')
+      .setValue('input[type=password]', 'testtest')
+      .click('input[type=submit]')
+      .waitForElementVisible('.navbar', 1000)
+      .getText('h1', function(res) {
+        this.assert.equal(res.value, 'Thanks for signing up!')
+      })
+
+    browser.assert.urlContains('/applications')
   },
 
-  'close': (client) => client.end()
+  'close': (browser) => browser.end()
 }
