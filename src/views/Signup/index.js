2af69ab8ddfa1c64f00d17338dfd8fbf6528b564
Yechiel K <chilikkal@gmail.com>
2017-05-02 20:37:18 -0400

    added styling for the sign up form

--- src/views/Signup/index.js
+++ src/views/Signup/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
 const Signup = () =>
-  <form className="signup_form">
-    <input type="text" placeholder="Username" />
-    <input type="password" placeholder="Password" />
-    <input type="submit" value="Create User" />
+<div className="uk-position-center">
+  <h2 class="uk-heading-line uk-text-center"><span>Sign Up:</span></h2>
+  <form className="uk-form-stacked">
+
+    <div className="uk-margin">
+        <label className="uk-form-label" for="username">Username</label>
+        <div className="uk-form-controls">
+            <input className="uk-input uk-form-width-medium" id="username" type="text" placeholder="Username" />
+        </div><br />
+        <label className="uk-form-label" for="password">Password</label>
+        <div className="uk-form-controls">
+            <input className="uk-input uk-form-width-medium" id="password" type="password" placeholder="Password" />
+        </div><br />
+        <input type="submit" className="uk-button uk-button-default" value="Create User" />
+    </div>
   </form>
+</div>
 
 export default Signup;
