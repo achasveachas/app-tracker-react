1773deac75c514799e96244c60ddd77c402d9e43
Yechiel K <chilikkal@gmail.com>
2017-05-06 23:55:33 -0400

    added actions to applications reducer

--- src/redux/modules/Applications/reducer.js
+++ src/redux/modules/Applications/reducer.js
@@ -9,11 +9,20 @@ export default (state = initialState, action) => {
     case 'ADD_APPLICATION':
       return {
         ...state,
+        state.applications.concat(action.application)
       }
 
     case 'EDIT_APPLICATION':
+      const application = state.applications.filter(a => a.id == action.application.id)[0]
+      const editedApplication = Object.assign({}, action.application)
       return {
         ...state,
+        state.applications: [
+              ...state.applications.slice(0, action.index),
+              editedApplication,
+              ...state.applications.slice(action.index + 1)
+            ]
+        }
       }
 
     case 'DELETE_APPLICATION':
