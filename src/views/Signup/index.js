490b29293bfca44071f453c2e6eb24c35b53330f
Yechiel K <chilikkal@gmail.com>
2017-05-06 23:34:18 -0400

    refactored a bit to try and get it working on heroku

--- src/views/Signup/index.js
+++ src/views/Signup/index.js
@@ -8,11 +8,11 @@ const Signup = () =>
     <form className="uk-form-stacked">
 
       <div className="uk-margin">
-          <label className="uk-form-label" for="username">Username</label>
+          <label className="uk-form-label" htmlFor="username">Username</label>
           <div className="uk-form-controls">
               <input className="uk-input uk-form-width-medium" id="username" type="text" placeholder="Username" />
           </div><br />
-          <label className="uk-form-label" for="password">Password</label>
+        <label className="uk-form-label" htmlFor="password">Password</label>
           <div className="uk-form-controls">
               <input className="uk-input uk-form-width-medium" id="password" type="password" placeholder="Password" />
           </div><br />
