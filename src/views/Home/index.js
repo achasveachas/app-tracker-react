ee727556982fb40ec87cf4fb35644cda956e8b92
Yechiel K <chilikkal@gmail.com>
2017-05-07 01:48:31 -0400

    set up the home screen

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
-const Home = () => <div>Home</div>
+const Home = () =>
+<div className="uk-position-center">
+  <h1 className="uk-heading-line uk-text-center"><span>Welcome To My Job Application Tracker</span></h1>
+  <h3 className="uk-heading-line uk-text-center"><span>Please <a className="uk-link-muted" href='/signup'>sign up</a> or <a className="uk-link-muted" href='/login'>log in</a> to use</span></h3>
+</div>
 
 export default Home;
