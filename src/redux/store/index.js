076167c930fff88907d81baa1ad6ba2625cdbf96
Yechiel K <chilikkal@gmail.com>
2017-05-08 22:02:17 -0400

    added redux-form to redux

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
+import { reducer as form } from 'redux-form'
 import auth from '../modules/Auth/reducer'
 import applications from '../modules/Applications/reducer'
 
 const reducers = combineReducers({
+  form,
   auth,
   applications
 })
