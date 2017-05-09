ab7ad74142b17442616a1cf0ab868400b92ec920
Yechiel K <chilikkal@gmail.com>
2017-05-08 20:01:30 -0400

    added redux thunk

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
 import auth from '../modules/Auth/reducer'
 import applications from '../modules/Applications/reducer'
 
@@ -7,7 +8,10 @@ const reducers = combineReducers({
   applications
 })
 
+const middleware = [thunk]
+
 export default createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(...middleware)
 )
