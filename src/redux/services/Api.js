e7dbc13525ced74ca24a713844ec303f5f656316
Yechiel K <chilikkal@gmail.com>
2017-05-08 20:37:31 -0400

    started working on API call methods, set up the background tasks

--- src/redux/services/Api.js
+++ src/redux/services/Api.js
@@ -1,3 +1,22 @@
 import fetch from 'isomorphic-fetch'
 
 const BASE_URL = 'https://sleepy-cove-46703.herokuapp.com/api/v1/'
+
+const token = JSON.parse(localStorage.getItem('token'))
+
+const headers =  {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer: ${token}`
+}
+
+const parseResponse = (response) => {
+  return response.json()
+    .then(json) => {
+      if (!response.ok) {
+        return Promise.reject(json.errors)
+      }
+
+      return json
+    }
+}
