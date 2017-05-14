9e58671467a61bc4682a2637ad6cf91e6310aea6
Yechiel K <chilikkal@gmail.com>
2017-05-14 00:06:16 -0400

    added error message to the 'Not Found' page

--- src/views/NotFound/index.js
+++ src/views/NotFound/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
-const NotFound = () => <div>Not Found</div>
+const NotFound = () =>
+  <div className="uk-position-center">
+    <h1 className="uk-heading-line uk-text-center"><span>Whoops! The page you are looking for could not be found.</span></h1>
+    <h3 className="uk-heading-line uk-text-center"><span>Please check your URL or press the back button to navigate to the page you need.<br />If this error keeps happening please contact the website administrator.</span></h3>
+  </div>
 
 export default NotFound;
