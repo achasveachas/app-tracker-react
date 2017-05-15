4f34683c73524216777ece8d71bf7ed8a36c3be0
Yechiel K <chilikkal@gmail.com>
2017-05-15 11:59:41 -0400

    added 'New Application' buttons to the top and buttom the the ApplicationsTable

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,11 +1,14 @@
 
 import React from 'react'
 import ApplicationsTable from '../components/ApplicationsTable'
+import NewApplicationButton from '../components/NewApplicationButton'
 
 const Dashboard = () =>
   <div>
     <h1 className="uk-heading-line uk-text-center uk-padding"><span>My Job Dashboard</span></h1>
+    <NewApplicationButton />
     <ApplicationsTable />
+    <NewApplicationButton />
   </div>
 
 
---
+++ src/views/components/NewApplicationButton/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const NewApplicationButton = () => <button className="uk-button uk-button-primary uk-margin-medium uk-margin-left">New Application</button>
+
+export default NewApplicationButton
