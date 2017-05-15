501fa1f295d8a0d2fb54c16e637b97ce44a7c90b
Yechiel K <chilikkal@gmail.com>
2017-05-15 01:35:11 -0400

    connected ApplicationTable to redux

--- src/views/components/ApplicationsTable/index.js
+++ src/views/components/ApplicationsTable/index.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
+
 import ApplicationRow from '../ApplicationRow'
 
 class ApplicationsTable extends Component {
@@ -31,4 +33,13 @@ class ApplicationsTable extends Component {
   )}
 }
 
-export default ApplicationsTable
+const mapStateToProps = (state) => {
+  return {
+    applications: state.applications.applications,
+    currentApplication: state.applications.currentApplication,
+    currentUser: state.auth.currentUser
+  }
+}
+
+
+export default connect(mapStateToProps)(ApplicationsTable)
