1917988f82e9e67a2308239699d32a012715b0c2
Yechiel K <chilikkal@gmail.com>
2017-05-14 23:59:12 -0400

    Stubbed out the table component

--- src/views/ApplicationsTable/index.js
+++ src/views/ApplicationsTable/index.js
@@ -1,8 +1,32 @@
 import React from 'react'
 
 const ApplicationsTable = () =>
-  <div>
-    <div><span>Applications Table</span></div>
+  <div className="uk-overflow-auto">
+    <table className="uk-table uk-table-hover uk-table-divider">
+      <thead>
+        <tr>
+          <th>Company</th>
+          <th>Date</th>
+          <th>Action</th>
+          <th>Completed</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Hello World</td>
+          <td>Hello World</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Hello World</td>
+          <td>Hello World</td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+
+    </table>
   </div>
 
 
