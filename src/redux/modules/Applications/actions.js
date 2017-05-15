ff355eda8fa7c5b8b36ec2ff72e96c694b844527
Yechiel K <chilikkal@gmail.com>
2017-05-15 10:05:42 -0400

    added async action to fetch applications

--- src/redux/modules/Applications/actions.js
+++ src/redux/modules/Applications/actions.js
@@ -1,3 +1,5 @@
+import ApiServices from '../../../services/Api'
+
 export const gotApplications = (applications) => {
   return {
     type: 'GOT_APPLICATIONS',
@@ -24,5 +26,18 @@ export const deleteApplication = (id) => {
   return {
     type: 'DELETE_APPLICATION',
     id: id
+  }
+}
+
+
+export const getApplications = (user_id) => {
+  return dispatch = {
+    return ApiServices.get(`/users/#{user_id}/applications`)
+      .then(response => {
+        dispatch(gotApplications(response.applications))
+      })
+      .catch(errors => {
+        console.log(errors);
+      })
   }
 }
