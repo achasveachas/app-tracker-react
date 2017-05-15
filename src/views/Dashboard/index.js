9b9eae0f1f86f2a5181094112e84d67634bbf305
Yechiel K <chilikkal@gmail.com>
2017-05-15 14:48:02 -0400

    added test modal as container for application form

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,5 +1,6 @@
-
 import React, { Component} from 'react'
+import Modal from 'react-modal'
+
 import ApplicationsTable from '../components/ApplicationsTable'
 import NewApplicationButton from '../components/NewApplicationButton'
 
@@ -9,17 +10,34 @@ class Dashboard extends Component {
     super(props)
 
     this.state = {
-      currentApplication: null
+      currentApplication: null,
+      modalIsOpen: false
     }
+
+    this.openModal = this.openModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+  }
+  openModal() {
+    this.setState({modalIsOpen: true})
+  }
+  closeModal() {
+    this.setState({modalIsOpen: false})
   }
 
   render() {
     return (
       <div>
         <h1 className="uk-heading-line uk-text-center uk-padding"><span>My Job Dashboard</span></h1>
-        <NewApplicationButton />
+        <NewApplicationButton onClick={this.openModal}/>
         <ApplicationsTable />
         <NewApplicationButton />
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          contentLabel="Modal">
+          <h1>Test Modal</h1>
+          <button type="button" onClick={this.closeModal}>Close</button>
+        </Modal>
+
       </div>
     )}
   }
--- src/views/components/NewApplicationButton/index.js
+++ src/views/components/NewApplicationButton/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const NewApplicationButton = () => <button className="uk-button uk-button-primary uk-margin-medium uk-margin-left">New Application</button>
+const NewApplicationButton = (props) => {
+  return (
+    <button
+    className="uk-button uk-button-primary uk-margin-medium uk-margin-left"
+    onClick={props.onClick}>
+      New Application
+    </button>
+  )
+}
 
 export default NewApplicationButton
