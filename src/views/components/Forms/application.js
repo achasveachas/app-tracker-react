0c4d93264d1222bc61ee458d7c1901e65990bd50
Yechiel K <chilikkal@gmail.com>
2017-05-16 22:49:02 -0400

    removed an unneeded dependency from application form

--- src/views/components/Forms/application.js
+++ src/views/components/Forms/application.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Field, reduxForm, initialize } from 'redux-form'
+import { Field, reduxForm } from 'redux-form'
 import { connect } from 'react-redux'
 
 const form = reduxForm({
