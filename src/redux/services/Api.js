48efa145ac7e97c31f5d569696527dd8e6a83888
Yechiel K <chilikkal@gmail.com>
2017-05-18 21:35:28 -0400

    removed a debugger that didn't belong

--- src/redux/services/Api.js
+++ src/redux/services/Api.js
@@ -32,7 +32,7 @@ export default {
 
   post(url, data ={}) {
     const body = JSON.stringify(data)
-    debugger
+    
     return fetch(`${BASE_URL}${url}`, {
       method: 'POST',
       headers: headers,
@@ -43,10 +43,9 @@ export default {
 
   patch(url, data ={}) {
     const body = JSON.stringify(data)
-    debugger
     return fetch(`${BASE_URL}${url}`, {
       method: 'PATCH',
-      headers: headers,
+      headers: 'headers',
       body: body
     })
     .then(parseResponse)
@@ -55,7 +54,7 @@ export default {
   delete(url) {
     return fetch(`${BASE_URL}${url}`, {
       method: 'DELETE',
-      headers: headers
+      headers: 'headers'
     })
     .then(parseResponse)
   }
