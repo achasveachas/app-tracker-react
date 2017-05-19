782e548a37c98eaa66563487cfd58ddefc7c2ccb
Yechiel K <chilikkal@gmail.com>
2017-05-19 15:30:44 -0400

    updated the url for the API

--- src/redux/services/Api.js
+++ src/redux/services/Api.js
@@ -1,6 +1,6 @@
 import fetch from 'isomorphic-fetch'
 
-const BASE_URL = 'https://sleepy-cove-46703.herokuapp.com/api/v1'
+const BASE_URL = 'https://app-tracker-api.herokuapp.com/api/v1'
 
 const parseResponse = (response) => {
   return response.json()
