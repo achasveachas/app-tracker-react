806a42c5a16e181e3c38f6e3819347ff925a340e
Yechiel K <chilikkal@gmail.com>
2017-05-21 13:34:02 -0400

    added confirmation dialogue before deleting applications

--- src/views/components/ApplicationRow/index.js
+++ src/views/components/ApplicationRow/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function ApplicationRow(props){
   const handleClick = () => props.onClick(props.application.id)
-  const handleDelete = () => props.onDelete(props.user_id, props.application.id)
+  const handleDelete = () => {
+    if (confirm("Are you sure you want to delete this application?\nThis action can not be undone.")) {
+      props.onDelete(props.user_id, props.application.id)
+    }
+  }
   let contact = ""
   if (props.application.contact_name && props.application.contact_title) {
     contact = props.application.contact_name + " - " + props.application.contact_title
