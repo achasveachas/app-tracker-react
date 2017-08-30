72a5077318b2bb0e75b54dde1b629a7a12441f6d
Yechiel Kalmenson <contact@yechiel.me>
2017-08-29 20:10:13 -0400

    moved all of the CSS into a CDN

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 
-import './styles/uikit.css';
 import App from './containers/App';
 import store from './redux/store'
 
