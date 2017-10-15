14ab731d61c5921df4be98e53ad0742d64110dc1
Yechiel Kalmenson <contact@yechiel.me>
2017-10-15 19:33:23 -0400

    create rudimentary about page

---
+++ src/views/About/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const  About = () =>
+    <div className="uk-position-center">
+      <h1 className="uk-heading-line uk-text-center"><span>WAbout This App</span></h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sollicitudin elementum sapien in commodo. Aliquam erat volutpat. Donec est sapien, tempor ac nisl at, rutrum porttitor augue. Sed a urna vel augue euismod pellentesque eu eget arcu. Fusce at quam molestie, posuere nisi vitae, dignissim odio. Donec luctus velit in urna ullamcorper cursus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus eu ex tincidunt, viverra justo vitae, dignissim felis. Mauris ultrices mattis lorem non venenatis. Integer nisl turpis, finibus id tellus id, finibus pharetra orci.
+
+        Nunc rhoncus fringilla molestie. Nulla interdum molestie elit, sed ultrices dui aliquam id. Proin eget eleifend dui. Quisque lobortis aliquet interdum. Suspendisse ullamcorper gravida tortor. Curabitur pellentesque luctus volutpat. In scelerisque tempor lacus id placerat. Ut consectetur metus sit amet ultrices euismod. In ut dapibus libero, et rhoncus purus. Donec id augue sapien. Aliquam eros lorem, sagittis sed justo eget, cursus ullamcorper enim. Nullam tortor ex, ornare et nibh ac, molestie scelerisque ex.
+
+        Etiam bibendum nec odio at mollis. Duis in feugiat nisl. Fusce ornare velit ut quam sagittis, ut tristique magna lobortis. Sed rhoncus tortor in pharetra volutpat. Nullam consectetur nibh vitae mi sodales interdum. Nullam vel sem quis neque elementum semper id non elit. Pellentesque ultrices scelerisque neque id vulputate. Nullam dignissim mattis mi rhoncus vehicula. 
+      </p>
+    </div>
+
+export default About
