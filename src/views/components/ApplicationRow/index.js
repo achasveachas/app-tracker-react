bcc3ac562922ee86d1ea553da822cedbf35a28cb
Yechiel Kalmenson <contact@yechiel.me>
2018-07-23 20:11:10 -0400

    refactor a global variable

--- src/views/components/ApplicationRow/index.js
+++ src/views/components/ApplicationRow/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ApplicationRow(props){
   const handleClick = () => props.onClick(props.application.id)
   const handleDelete = () => {
-    if (confirm("Are you sure you want to delete this application?\nThis action can not be undone.")) {
+    if (window.confirm("Are you sure you want to delete this application?\nThis action can not be undone.")) {
       props.onDelete(props.user_id, props.application.id)
     }
   }
